8bd7f98e41dc3dd78fbf43049c0be94b3ed1ef40
jonathan_daniel <danieljonathan560@gmail.com>
2020-10-01 09:06:20 +0200

    creation models, add mongoose and retrieval of information from forms

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Sauce = require('./models/Sauce');
+const User = require('./models/User');
+
+mongoose.connect('mongodb+srv://JoFDaniel:Ikigai56@cluster1.gtwm5.mongodb.net/P6-piquante?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
@@ -9,12 +20,51 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    console.log('requête reçue !');
-    next();
+app.use(bodyParser.json());
+
+app.post('/api/sauces', (req, res, next) => {
+    console.log(req.body);
+    delete req.body._id;
+    const sauce = new Sauce({
+        ...req.body
+    });
+    sauce.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+    .catch(error => res.status(400).json({ error })) 
 });
+
+app.post('/api/auth/login', (req, res, next) => {
+    console.log(req.body);
+    delete req.body._id;
+    const user = new User({
+        ...req.body
+    });
+    user.save()
+    .then(() => res.status(201).json({ message: 'utilisateur enregistré !'}))
+    .catch(error => res.status(400).json({ error }))
+}); 
+
+app.use('/api/sauces', (req, res, next) => {
+    const sauces = [
+        {
+        _id: 'oimjoijlhui',
+        name: 'Black Garlic',
+        manufacturer: 'Bravado Spice Company',
+        description: 'Team Bravado is back at it with an elevated offering where Carolina Reaper meets aged black garlic. The sweetness of the slowly cooked garlic tempers the initial bitter burn of the Reaper, but not for long... This is a biting hot sauce you\'ll want in marinades, sauces, dressings, and on those garlic wings! ',
+        heat: 6,
+        likes: 100,
+        dislikes: 0,
+        imageUrl: 'https://www.chilliworld.com/content/images/thumbs/0000827_blairs-ultra-death-sauce-in-a-coffin_550.jpeg',
+        mainPepper: 'Carolina Reaper',
+        usersLiked: [],
+        usersDisliked: []
+        },
+    ];
+    res.status(200).json(sauces);
+}); 
+
 app.use((req, res) => {
-    res.json({ message: 'Votre requêtea bien été reçue !'});
+    res.json({ message: 'Votre requête a bien été reçue !'});
 });
 
 module.exports = app;
