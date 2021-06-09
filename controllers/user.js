7af5c878e40c57008dcebe77bd5e2c9fefdc7795
Jonathan - BC <danieljonathan560@gmail.com>
2021-06-09 18:18:45 +0200

    modify .gitignore and user controller

--- controllers/user.js
+++ controllers/user.js
@@ -42,16 +42,17 @@ exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
       .then(user => {
         if (!user) {
-          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+          return res.status(401).json({ message: 'Utilisateur non trouvé !' });
         }
         bcrypt.compare(req.body.password, user.password)
           .then(valid => {
             if (!valid) {
               return res.status(401).json({ error: 'Mot de passe incorrect !' });
             }
+            const email = maskEmail(req.body.email)
             res.status(200).json({
               userId: user._id,
-              email: maskEmail(req.body.email),
+              email,
               token:  jwt.sign(
                 { userId: user._id },
                 process.env.JWT_SECRET,
